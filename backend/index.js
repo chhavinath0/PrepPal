992746af019c812f71263d148858cea3d0b49d59
RUETATA <138284511+RUETATA@users.noreply.github.com>
2025-10-03 23:19:38 +0530

    created mongoose models and defined all the routes needed in index.js

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const resumeRoutes = require("./routes/resume");
+const questionRoutes = require("./routes/questions");
+const feedbackRoutes = require("./routes/feedback");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", resumeRoutes);
+app.use("/api", questionRoutes);
+app.use("/api", feedbackRoutes);
+
+app.get("/",(req,res)=>{
+  res.json({message: 'Welcome'});
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch(err => console.error("❌ MongoDB connection error:", err));
+
---
+++ backend/routes/questions.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const { generateContent } = require("../utils/gemini");
+
+const router = express.Router();
+
+router.post("/get-questions", async (req, res) => {
+  const { resumeText, role, difficulty,duration } = req.body;
+
+  if (!resumeText || !role || !difficulty) {
+    return res.status(400).json({ success: false, message: "Missing fields" });
+  }
+
+  const prompt = `
+You are called PrepPal and you are taking an interview for a ${role} Software Engineer fresher role.
+
+Here is the candidate’s resume:
+${resumeText}
+
+Ask any number of ${difficulty}-level interview questions (mix of technical and behavioral) so that the interview is of around ${duration} minutes.
+Return each question as a numbered list.
+Number the questions clearly in a list.Just output the questions and the first question for introducing himself and also introduce yourself before that.
+Only Output Questions and answers.
+Don't output the reason for question or which type of question it is.
+`;
+
+  try {
+    const output = await generateContent(prompt);
+    const questions = output
+      .split("\n")
+      .filter((line) => line.trim())
+      .map((q) => q.replace(/^\d+\.\s*/, "").trim());
+
+    return res.json({ success: true, questions });
+  } catch (err) {
+    console.error("Gemini error:", err);
+    return res.status(500).json({ success: false, message: "Failed to generate questions" });
+  }
+});
+
+module.exports = router;
